748287fff7b3545f49280271227a03cc7db1798c
Ola Kveli <olakveli@yahoo.com>
2019-02-15 23:27:15 +0100

    adding packages and fixing poms, working sort of

--- eureka/src/main/kotlin/no/westerdals/eureka/EurekaApplication.kt
+++ eureka/src/main/kotlin/no/westerdals/eureka/EurekaApplication.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
-@SpringBootApplication(scanBasePackages = ["no.westerdals.eureka"])
+@SpringBootApplication(scanBasePackages = ["no.westerdals.gateway"])
 @EnableEurekaServer
-class EurekaApplication {}
+class EurekaApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(EurekaApplication::class.java, *args)
